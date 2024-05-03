2f571de1cb638fc7124e9ab54a59f37876cade8c
henrick404 <160244828+henrick404@users.noreply.github.com>
2024-05-03 09:41:44 -0300

    Add files via upload

---
+++ umsrit.js
@@ -0,0 +1,28 @@
+n1 = 1
+
+while (n1 <= 10) {
+    console.log(n1)
+    n1= n1 + 1
+}
+
+n2 = 10
+
+while (n2 > 0) {
+    console.log(n2)
+    n2= n2 - 1
+}
+n3 = 0
+window.prompt("número",n3)
+while (n3> 0) {
+    console.log(n3)
+    n3= n3 - 1
+}
+
+t1 = 0
+n4 = 0
+window.prompt("número2",t1)
+while (n4<= t1) {
+    console.log(n4)
+    n4= n4 + 1
+}
+
